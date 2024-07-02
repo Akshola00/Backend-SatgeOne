c916dccc3943ce3052017633107997ebbff4a5dc
Akshola00 <akinniyishola07@gmail.com>
2024-07-02 13:38:37 +0100

    Changes to be committed: 	modified:   build_files.sh 	modified:   vercel.json

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,23 @@
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
+#!/bin/bash
+
+# Exit the script if any command fails
+set -e
+
+# Create and activate a virtual environment
+python3.9 -m venv venv
+source venv/bin/activate
+
+# Upgrade pip
+pip install --upgrade pip
+
+# Install the dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python3.9 manage.py collectstatic --noinput
+
+# Create the output directory if it doesn't exist
+mkdir -p staticfiles_build
+
+# Move the collected static files to the output directory
+mv static/* staticfiles_build/
